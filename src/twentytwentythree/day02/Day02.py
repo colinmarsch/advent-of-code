40b58f99241b9630dc419bca1efceb66f299ce73
Colin Marsch <cmarsch@squareup.com>
2023-12-01 21:21:33 -0800

    Add python solution for 2023 day 2

--- src/twentytwentythree/day02/Day02.py
+++ src/twentytwentythree/day02/Day02.py
@@ -4,10 +4,48 @@ with open("Day02_input.txt") as file:
         lines.append(line.rstrip())
 
 def part1(lines):
-  pass
+  sum = 0
+  for line in lines:
+     possible = True
+     parts = line.split(":")
+     id = int(parts[0].split()[1])
+     sets = parts[1].split(";")
+     for set in sets:
+        cubes = set.split(",")
+        for choice in cubes:
+           colour = choice.split()[1]
+           count = int(choice.split()[0])
+           if colour == "red" and count > 12:
+              possible = False
+           if colour == "blue" and count > 14:
+              possible = False
+           if colour == "green" and count > 13:
+              possible = False
+     if possible:
+        sum += id
+  return sum
 
 def part2(lines):
-  pass
+  sum = 0
+  for line in lines:
+     greenMin = 0
+     blueMin = 0
+     redMin = 0
+     parts = line.split(":")
+     sets = parts[1].split(";")
+     for set in sets:
+        cubes = set.split(",")
+        for choice in cubes:
+           colour = choice.split()[1]
+           count = int(choice.split()[0])
+           if colour == "red" and count > redMin:
+              redMin = count
+           if colour == "blue" and count > blueMin:
+              blueMin = count
+           if colour == "green" and count > greenMin:
+              greenMin = count
+     sum += greenMin * blueMin * redMin
+  return sum
 
 print("Part 1: " + str(part1(lines)))
 print("Part 2: " + str(part2(lines)))
