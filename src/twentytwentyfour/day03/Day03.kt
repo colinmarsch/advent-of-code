a0b594a5dfc3b7406e2e6e2cb521bd3fa244dc5c
Colin Marsch <cmarsch@squareup.com>
2024-12-02 23:39:35 -0800

    2024 day 3 solved

--- src/twentytwentyfour/day03/Day03.kt
+++ src/twentytwentyfour/day03/Day03.kt
@@ -1,14 +1,68 @@
 package twentytwentyfour.day03
 
 import readInput
+import kotlin.math.min
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        return input.sumOf { line ->
+            var sum = 0L
+            for (i in line.indices) {
+                if (i < line.length - 8) {
+                    if (line.substring(i, i + 4) == "mul(") {
+                        val firstNumString = line.substring(i + 4, i + 7).split(",")[0]
+                        val firstNum = firstNumString.toLongOrNull() ?: continue
+                        val secondNumString = line.substring(
+                            i + 5 + firstNumString.length,
+                            min(i + 8 + firstNumString.length, line.length - 1)
+                        ).split(")")[0]
+                        val secondNum = secondNumString.toLongOrNull() ?: continue
+
+                        if (line[i + 5 + firstNumString.length + secondNumString.length] != ')') continue
+
+                        sum += firstNum * secondNum
+                    }
+                }
+            }
+            return@sumOf sum
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var process = true
+        return input.sumOf { line ->
+            var sum = 0L
+            for (i in line.indices) {
+                if (i < line.length - 8) {
+                    if (line.substring(i, i + 4) == "mul(") {
+                        val firstNumString = line.substring(i + 4, i + 7).split(",")[0]
+                        val firstNum = firstNumString.toLongOrNull() ?: continue
+                        val secondNumString = line.substring(
+                            i + 5 + firstNumString.length,
+                            min(i + 8 + firstNumString.length, line.length - 1)
+                        ).split(")")[0]
+                        val secondNum = secondNumString.toLongOrNull() ?: continue
+
+                        if (line[i + 5 + firstNumString.length + secondNumString.length] != ')') continue
+
+                        if (process) {
+                            sum += firstNum * secondNum
+                        }
+                    }
+                }
+                if (i < line.length - 7) {
+                    if (line.substring(i, i + 7) == "don't()") {
+                        process = false
+                    }
+                }
+                if (i < line.length - 4) {
+                    if (line.substring(i, i + 4) == "do()") {
+                        process = true
+                    }
+                }
+            }
+            return@sumOf sum
+        }
     }
 
     val input = readInput("twentytwentyfour/day03", "Day03_input")
