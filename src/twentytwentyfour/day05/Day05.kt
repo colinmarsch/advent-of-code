423b0849d11d9b34e2d3d341707099e50d739df6
Colin Marsch <cmarsch@squareup.com>
2024-12-04 23:56:55 -0800

    2024 day 5 solution

--- src/twentytwentyfour/day05/Day05.kt
+++ src/twentytwentyfour/day05/Day05.kt
@@ -4,11 +4,79 @@ import readInput
 
 fun main() {
   fun part1(input: List<String>): Long {
-    return 0
+    val orderingRules = input.filter { it.contains("|") }
+    val afterMap = mutableMapOf<Int, MutableSet<Int>>()
+
+    for (rule in orderingRules) {
+      val parts = rule.split("|")
+      val before = parts[0].toInt()
+      val after = parts[1].toInt()
+      afterMap[before] = afterMap.getOrDefault(before, mutableSetOf()).apply { add(after) }
+    }
+
+    val pageUpdates = input.filter { it.contains(",") }
+
+    var sum = 0L
+    for (update in pageUpdates) {
+      val parts = update.split(",")
+      val seenPages = mutableSetOf<Int>()
+      var validUpdate = true
+      for (part in parts) {
+        val pageNumber = part.toInt()
+        val invalidBeforePages = afterMap.getOrDefault(pageNumber, mutableSetOf())
+        if (seenPages.any { invalidBeforePages.contains(it) }) {
+          validUpdate = false
+          break
+        }
+        seenPages.add(pageNumber)
+      }
+      if (validUpdate) {
+        sum += parts[parts.size / 2].toInt()
+      }
+    }
+    return sum
   }
 
   fun part2(input: List<String>): Long {
-    return 0
+    val orderingRules = input.filter { it.contains("|") }
+    val afterMap = mutableMapOf<Int, MutableSet<Int>>()
+
+    for (rule in orderingRules) {
+      val parts = rule.split("|")
+      val before = parts[0].toInt()
+      val after = parts[1].toInt()
+      afterMap[before] = afterMap.getOrDefault(before, mutableSetOf()).apply { add(after) }
+    }
+
+    val pageUpdates = input.filter { it.contains(",") }
+
+    var sum = 0L
+    for (update in pageUpdates) {
+      val parts = update.split(",")
+      val seenPages = mutableSetOf<Int>()
+      var validUpdate = true
+      for (part in parts) {
+        val pageNumber = part.toInt()
+        val invalidBeforePages = afterMap.getOrDefault(pageNumber, mutableSetOf())
+        if (seenPages.any { invalidBeforePages.contains(it) }) {
+          validUpdate = false
+          break
+        }
+        seenPages.add(pageNumber)
+      }
+
+      if (!validUpdate) {
+        val pageUpdateSet = parts.map { it.toInt() }.toMutableSet()
+        val validPageOrder = mutableListOf<Int>()
+        while (pageUpdateSet.isNotEmpty()) {
+          val nextItem = pageUpdateSet.find { afterMap[it]?.none { it in pageUpdateSet } == true }
+          validPageOrder.add(nextItem!!)
+          pageUpdateSet.remove(nextItem)
+        }
+        sum += validPageOrder[validPageOrder.size / 2]
+      }
+    }
+    return sum
   }
 
   val input = readInput("twentytwentyfour/day05", "Day05_input")
